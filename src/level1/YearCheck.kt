9ee43be140f247b5aa317488d9b915132d8a3bfa
JinHo <jinho6187@gmail.com>
2020-01-25 23:44:35 +0900

    년도 체크 문제 풀이

--- src/level1/YearCheck.kt
+++ src/level1/YearCheck.kt
@@ -8,11 +8,18 @@ class YearCheck {
     fun solution(a: Int, b: Int): String {
         var answer = ""
 
+        var day = 0
+        val month = intArrayOf(0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30)
         val list = arrayOf("FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU")
 
-        val day = b % 7
+        for (i in 0 until a) {
+            day += month[i]
+        }
+        day += b
 
-        return list[day - 1]
+        val index = (day % 7) - 1
+        if (index == -1) return "THU"
+        return list[index]
     }
 
 }
@@ -20,7 +27,11 @@ class YearCheck {
 fun main() {
 
     val yearCheck = YearCheck()
-    yearCheck.solution(5, 24).assertEquals("TUE")
+    yearCheck.solution(1, 2).assertEquals("SAT")
+    yearCheck.solution(1, 5).assertEquals("TUE")
     yearCheck.solution(2, 29).assertEquals("MON")
+    yearCheck.solution(5, 24).assertEquals("TUE")
+    yearCheck.solution(9, 10).assertEquals("SAT")
+    yearCheck.solution(12, 15).assertEquals("THU")
 
 }
