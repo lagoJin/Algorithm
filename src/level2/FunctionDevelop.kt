10b935bb6755ae348f70ef0ad9654b246c1a36d5
jinho <jinho6187@gmail.com>
2020-01-26 18:28:30 +0900

    개발 기간 계산 - 날짜 계산 ✓

--- src/level2/FunctionDevelop.kt
+++ src/level2/FunctionDevelop.kt
@@ -2,6 +2,7 @@ package level2
 
 import assertEquals
 import java.util.*
+import kotlin.collections.HashMap
 
 class FunctionDevelop {
 
@@ -11,36 +12,23 @@ class FunctionDevelop {
 
         var count = 0
 
+
         for (i in progresses.indices) {
             if (progresses[i] == speeds[i]) {
                 queue.add(count + 1)
             } else {
                 for (j in (progresses[i])..100 step speeds[i]) {
                     count++
-                    if (j == 100) {
-                        queue.add(count - 1)
-                        count = 0
-                    }
                 }
+                queue.add(count - 1)
+                count = 0
             }
         }
 
-        val list = ArrayList<Int>()
         println(queue.toString())
+        val list = ArrayList<Int>()
 
         var day = 1
-        for (i in queue.indices) {
-            if (i + 1 == queue.size) {
-                list.add(1)
-            } else {
-                if (queue[i] > queue[i + 1]) {
-                    day++
-                } else {
-                    list.add(day)
-                    day = 1
-                }
-            }
-        }
 
         return list.toIntArray()
     }
@@ -50,7 +38,8 @@ class FunctionDevelop {
 fun main() {
 
     val functionDevelop = FunctionDevelop()
-    //functionDevelop.solution(intArrayOf(93, 30, 55), intArrayOf(1, 30, 5)).assertEquals(intArrayOf(2, 1))
-    functionDevelop.solution(intArrayOf(21, 25, 20), intArrayOf(5, 5, 5)).assertEquals(intArrayOf(3))
+
+    functionDevelop.solution(intArrayOf(55, 90, 30), intArrayOf(5, 1, 10)).assertEquals(intArrayOf(1, 2))
+    functionDevelop.solution(intArrayOf(93, 30, 55), intArrayOf(1, 30, 5)).assertEquals(intArrayOf(2, 1))
 
 }
